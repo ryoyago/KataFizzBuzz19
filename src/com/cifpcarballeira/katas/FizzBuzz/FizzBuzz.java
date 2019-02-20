46a2375af2cdfc51e606ccec6678beb458c01288
Yago Garcia <ryoyago@outlook.com>
2019-02-20 10:13:58 +0100

    1 devuelve 1

---
+++ src/com/cifpcarballeira/katas/FizzBuzz/FizzBuzz.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.cifpcarballeira.katas.FizzBuzz;
+
+/**
+ *
+ * @author dam210
+ */
+public class FizzBuzz {
+
+    public String getFizzBuzz(int i) {
+        return "1";
+    }
+
+    
+    
+}
--- test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
+++ test/com/cifpcarballeira/katas/FizzBuzz/Test/TestFizzBuzz.java
@@ -6,6 +6,7 @@ package com.cifpcarballeira.katas.FizzBuzz.Test;
  * and open the template in the editor.
  */
 
+import com.cifpcarballeira.katas.FizzBuzz.FizzBuzz;
 import org.junit.Test;
 import static org.junit.Assert.*;
 
@@ -18,9 +19,11 @@ public class TestFizzBuzz {
     public TestFizzBuzz() {
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
+
+     @Test
+     public void test1devuelve1() {
+     FizzBuzz fb= new FizzBuzz();
+     String resultado = fb.getFizzBuzz(1);
+     assertEquals("1",resultado);
+     }
 }
